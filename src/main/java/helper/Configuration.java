6ba411d7d895a8d9467225dd5e5321d29a273d9e
Sebastian Falana <s.falana@azagroup.eu>
2023-02-16 19:03:03 +0100

    Add

--- src/main/java/helper/Configuration.java
+++ src/main/java/helper/Configuration.java
@@ -1,4 +1,21 @@
 package helper;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import java.util.Properties;
+
+import static jdk.internal.net.http.common.Utils.getProperty;
+
+
 public class Configuration {
+
+    public static String getBaseUrl() {
+        return getProperty("baseUrl");
+    }
+
+    public static String getPrivateKey() {
+        return getProperty("privateKey");
+    }
 }
